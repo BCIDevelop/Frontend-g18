8940843149c4603a659cd61910871e37823268ff
Luis Lopez <luis.lopez@utec.edu.pe>
2022-11-10 19:22:53 -0500

    login test

--- semana6/inputAnimation/facebook.js
+++ semana6/inputAnimation/facebook.js
@@ -1,5 +1,47 @@
 function checkLoginState() {
     FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+      console.log(response)
+      if (response.status === 'connected') {  
+  
+            // Logged into your app and Facebook.
+            datos();    
+          }
     });
+  
+    function datos() {
+  
+          FB.api('/me?fields=name,email,picture', function(response) {
+  
+  
+             console.log(response)
+             const name=response.name;
+             const email=response.email;
+             const picture=response.picture.data.url;
+             const id=response.id;
+  
+             var xhr = new XMLHttpRequest();
+             xhr.open('POST', 'http://localhost:3000/loginFB');
+             xhr.setRequestHeader('Content-Type', 'application/json');
+             xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+             xhr.responseType = 'json';
+             xhr.send(JSON.stringify({'name' :name ,'email' : email ,'id' : id ,'picture' : picture}));
+             xhr.onload = function() {
+  
+                if(xhr.response.errorXHR)
+                  { 
+  
+                   overlayError=document.getElementById('overlayError')
+                   mensajeError=document.getElementById('mensajeError')
+                   mensajeError.innerHTML=xhr.response.errorXHR 
+                   overlayError.style.visibility='visible'
+                  }
+                  else{
+                    if (xhr.response.result == 'redirect') {
+                       //redirecting to main page from here.
+                       window.location.replace(xhr.response.url);
+                      }
+                  }
+              };
+          })
+      }    
   }
